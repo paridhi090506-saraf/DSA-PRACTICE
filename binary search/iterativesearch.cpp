d2c270f0931d6b6eeb69cb651ce7ac67d9286931
paridhi090506-saraf <paridhi090506@gmail.com>
2026-06-07 13:54:18 +0530

    Lower bound implementation

--- binary search/iterativesearch.cpp
+++ binary search/iterativesearch.cpp
@@ -21,7 +21,7 @@ int main() {
     int low = 0, high = n - 1;
 
     while(low <= high) {
-        int mid = low + (high - low) / 2;
+        int mid = low + high / 2;
 
         if(arr[mid] == target) {
             cout << "Found";
---
+++ binary search/lowerbound.cpp
@@ -0,0 +1,32 @@
+//find index such that arr[index]>=x
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter the number of elements in an array"<<endl;
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int x;
+    cout<<"Enter lower bound value";
+    cin>>x;
+    int low=0;
+    int high=n-1;
+    int ans=n;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[mid]>=x)
+        {
+            high=mid-1;
+            ans=mid;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    cout<<"Index is "<<ans<<endl;
+}
