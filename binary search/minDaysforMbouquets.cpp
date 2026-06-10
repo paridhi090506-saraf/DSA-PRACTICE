f7e0c1f897c46dade6a18b1caf0908c71d4734e6
paridhi090506-saraf <paridhi090506@gmail.com>
2026-06-10 23:00:35 +0530

    added optimal method to find minimum days for m bouquets

--- binary search/minDaysforMbouquets.cpp
+++ binary search/minDaysforMbouquets.cpp
@@ -34,6 +34,60 @@ int main(){
     cout<<"Enter the number of adjacent flowers required"<<endl;
     cin>>k;
     if(n < m * k){
+        cout << -1 << endl;
+        return 0;
+    }
+    int mini = *min_element(arr.begin(), arr.end());
+    int maxi = *max_element(arr.begin(), arr.end());
+    int low=mini;
+    int high=maxi;
+    int ans=-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(bouquet(arr,mid,m,k)){
+            ans=mid;
+            high=mid-1;;
+        }
+        else
+        low=mid+1;
+    }
+    cout<<"Minimum number of days="<<ans<<endl;
+    return 0;
+}
+
+//BRUTE
+/*bool bouquet(vector<int> & arr, int days,int m,int k){
+    int count=0,noB=0;
+    for(int i=0;i<arr.size();i++){
+        if(arr[i]<=days){
+            count++;
+        }
+        else{
+            noB+=(count/k);
+            count=0;
+        }
+    }
+    noB+=(count/k);
+    if(noB>=m)
+    return true;
+    else
+    return false;
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements in array"<<endl;
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the elements of array"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    int m,k;
+    cout<<"Enter the number of bouquets"<<endl;
+    cin>>m;
+    cout<<"Enter the number of adjacent flowers required"<<endl;
+    cin>>k;
+    if(n < m * k){
     cout << -1 << endl;
     return 0;
 }
@@ -44,7 +98,7 @@ int main(){
             cout<<"Minimum days="<<i<<endl;
             return 0;
         }
-    }
+    } 
     return -1;
 
-}
+}*/
