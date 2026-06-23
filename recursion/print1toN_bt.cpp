2700a185f828dec6f40c8f85287782932c49035a
paridhi090506-saraf <paridhi090506@gmail.com>
2026-06-23 23:03:13 +0530

    Backtrack

---
+++ recursion/print1toN_bt.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void f(int i,int n){
+    if(i<1)
+    return;
+    f(i-1,n);
+    cout<<i<<endl;
+}
+int main(){
+    int n;
+    cout<<"Enter value of n";
+    cin>>n;
+    f(n,n);
+    return 0;
+}
