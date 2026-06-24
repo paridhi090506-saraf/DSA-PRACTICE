609ebacb6efed5930d570acfd514c8060529c9ba
paridhi090506-saraf <paridhi090506@gmail.com>
2026-06-24 21:29:43 +0530

    Palindrome string using recursion

---
+++ recursion/stringpalindrome.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+bool palin(int i,string& s){
+    if(i>=(s.size()/2))
+    return true;
+    if(s[i]!=s[s.size()-i-1])
+    return false;
+    return palin(i+1,s);
+}
+int main(){
+    string s;
+    cout<<"Enter a string";
+    cin>>s;
+    if(palin(0, s))
+        cout << "Palindrome";
+    else
+        cout << "Not Palindrome";
+
+}
