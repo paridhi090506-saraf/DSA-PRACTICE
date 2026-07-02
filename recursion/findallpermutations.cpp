ee1c4b8cabd196615cf9d5ee0f111cdc503aac8d
paridhi090506-saraf <paridhi090506@gmail.com>
2026-07-02 19:36:53 +0530

    Permutations

--- recursion/findallpermutations.cpp
+++ recursion/findallpermutations.cpp
@@ -1,6 +1,38 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+void permute(int ind,vector<int> & arr, vector<vector<int>> & ans){
+    if(ind==arr.size()){
+        ans.push_back(arr);
+        return;
+    }
+    for(int i=ind;i<arr.size();i++){
+        swap(arr[i],arr[ind]);
+        permute(ind+1,arr,ans);
+        swap(arr[i],arr[ind]);
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the number of elements of array";
+    cin>>n;
+    vector<int> arr(n);
+    cout<<"Enter the elemts of an array";
+    for(int i=0;i<n;i++)
+    cin>>arr[i];
+    vector<vector<int>> ans;
+    permute(0,arr,ans);
+    for(auto it:ans){
+        for(auto ele:it)
+        cout<<ele<<" ";
+        cout<<endl;
+    }
+}
+
+
+/*#include<iostream>
+#include<vector>
+using namespace std;
 void permute(vector<int> & arr,vector<int> & ds, vector<vector<int>> & ans, int freq[]){
     if(ds.size()==arr.size()){
     ans.push_back(ds);
@@ -33,4 +65,4 @@ int main(){
         cout<<ele<<" ";
         cout<<endl;
     }
-}
+}*/
