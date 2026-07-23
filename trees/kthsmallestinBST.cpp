a48e7df4013a4af01a927a266e09604931381717
paridhi090506-saraf <paridhi090506@gmail.com>
2026-07-23 19:02:59 +0530

    Kth smallest in BST

--- trees/kthsmallestinBST.cpp
+++ trees/kthsmallestinBST.cpp
@@ -40,7 +40,7 @@ int main(){
     return 0;
 }
 
-
+//ITERATIVE
 /*#include<iostream>
 #include<stack>
 using namespace std;
@@ -58,4 +58,60 @@ int kthSmallest(Node* root, int k){
             return root->data;
         root=root->right;
     }
+}*/
+
+//MORRIS
+/*#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int value){
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};
+int kthSmallest(Node* root, int k){
+    int count = 0;
+    Node* curr = root;
+    while(curr != NULL){
+        if(curr->left == NULL){
+            count++;
+            if(count == k)
+                return curr->data;
+            curr = curr->right;
+        }
+        else{
+            Node* prev = curr->left;
+            while(prev->right != NULL && prev->right != curr){
+                prev = prev->right;
+            }
+            if(prev->right == NULL){
+                prev->right = curr;
+                curr = curr->left;
+            }
+            else{
+                prev->right = NULL;
+                count++;
+                if(count == k)
+                    return curr->data;
+                curr = curr->right;
+            }
+        }
+    }
+    return -1;   // k is greater than number of nodes
+}
+int main(){
+    Node* root = new Node(5);
+    root->left = new Node(3);
+    root->right = new Node(7);
+    root->left->left = new Node(2);
+    root->left->right = new Node(4);
+    root->right->left = new Node(6);
+    root->right->right = new Node(8);
+    int k = 4;
+    cout << "Kth Smallest = " << kthSmallest(root, k);
+    return 0;
 }*/
