59e36e82426c6155acc5db13b6f2cd9b24f547f5
paridhi090506-saraf <paridhi090506@gmail.com>
2026-07-31 03:12:17 +0530

    Number of distinct islands

---
+++ graphs/numberofDistinctIslands.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+void dfs(int row,int col,vector<vector<int>>& vis,vector<vector<int>> & grid,vector<pair<int,int>>&vec,int row0,int col0){
+    vis[row][col]=1;
+    vec.push_back({row-row0,col-col0});
+    int n=grid.size();
+    int m=grid[0].size();
+    int delRow[]={-1,0,+1,0};
+    int delCol[]={0,-1,0,+1};
+    for(int i=0;i<4;i++){
+        int nr=row+delRow[i];
+        int nc=col+delCol[i];
+        if(nr>=0 && nr<n && nc>=0 && nc<m && !vis[nr][nc] && grid[nr][nc]==1){
+            dfs(nr,nc,vis,grid,vec,row0,col0);
+        }
+    }
+}
+int countDistinctIslands(vector<vector<int>> & grid){
+    int n=grid.size();
+    int m=grid[0].size();
+    vector<vector<int>> vis(n,vector<int>(m,0));
+    set<vector<pair<int,int>>> st;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(!vis[i][j] && grid[i][j]==1){
+                vector<pair<int,int>> vec;
+                dfs(i,j,vis,grid,vec,i,j);
+                st.insert(vec);
+            }
+        }
+    }
+    return st.size();
+}
+int main() {
+
+    vector<vector<int>> grid = {
+        {1,1,0,1,1},
+        {1,0,0,0,0},
+        {0,0,0,0,1},
+        {1,1,0,1,1},
+        {1,0,0,0,0}
+    };
+
+    cout << "Number of distinct islands: "
+         << countDistinctIslands(grid) << endl;
+
+    return 0;
+}
